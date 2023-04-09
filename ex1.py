4005fdfb91dbf5cd28469ea4d8c5f023a370678f
LucasStapf <lucas.c.stapf@gmail.com>
2023-04-09 20:12:08 -0300

    Alterada a plotagem dos gráficos.

--- ex1.py
+++ ex1.py
@@ -4,10 +4,10 @@ from matplotlib import pyplot as plt
 fc = 10**6 #frequencia da portadora
 fm = 10**4 #frequencia da moduladora
 
-ac = 1 #amplitude da portadora
+ac = 10 #amplitude da portadora
 am = 1 #amplitude da moduladora
 
-k = 1 #fator de sensibilidade
+k = 0.6 #fator de sensibilidade
 
 def portadora(t):
     return ac * np.cos(2 * np.pi * fc * t)
@@ -15,10 +15,30 @@ def portadora(t):
 def moduladora(t):
     return am * np.cos(2 * np.pi * fm * t)
 
-def forma_padrao(t):
+def sinal_am(t):
     return (1 + k * moduladora(t)) * portadora(t)
 
-t = np.linspace(-100, 100, 2000)
+t = np.linspace(0, 0.001, 10 ** 7)
 
-plt.plot(t, portadora(t), color='red')
+
+plt.subplot(3, 1, 1)
+plt.plot(portadora(t), 'r')
+plt.ylabel('Amplitude')
+plt.xlabel('Portadora')
+plt.xlim(0, 0.001)
+
+plt.subplot(3, 1, 2)
+plt.plot(moduladora(t), 'g')
+plt.ylabel('Amplitude')
+plt.xlabel('Moduladora')
+plt.xlim(0, 0.001)
+
+plt.subplot(3, 1, 3)
+plt.plot(sinal_am(t), 'b')
+plt.ylabel('Amplitude')
+plt.xlabel('Sinal AM')
+plt.xlim(0, 0.001)
+
+# plt.plot(t, portadora(t), color='red')
+plt.subplots_adjust(hspace=1)
 plt.show()
