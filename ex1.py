095cf1eb12311252988ba39997879552ac1350a7
LucasStapf <lucas.c.stapf@gmail.com>
2023-04-10 08:01:24 -0300

    Ajustes na plotagem

--- ex1.py
+++ ex1.py
@@ -9,6 +9,13 @@ am = 1 #amplitude da moduladora
 
 k = 0.6 #fator de sensibilidade
 
+def vetor_tempos(f, ciclos):
+    fs = 2 * 10 * f             # freq. de amostragem
+    N = int(round(ciclos * (fs / f)))
+    t_f = ciclos * (1 / f)
+    A = int(t_f * fs)           # nro de amostras
+    return np.linspace(0, t_f, A)
+
 def portadora(t):
     return ac * np.cos(2 * np.pi * fc * t)
 
@@ -18,26 +25,25 @@ def moduladora(t):
 def sinal_am(t):
     return (1 + k * moduladora(t)) * portadora(t)
 
-t = np.linspace(0, 0.001, 10 ** 7)
-
+t = vetor_tempos(fc, 200)
 
 plt.subplot(3, 1, 1)
 plt.plot(portadora(t), 'r')
 plt.ylabel('Amplitude')
 plt.xlabel('Portadora')
-plt.xlim(0, 0.001)
+#plt.xlim(0, xlim)
 
 plt.subplot(3, 1, 2)
 plt.plot(moduladora(t), 'g')
 plt.ylabel('Amplitude')
 plt.xlabel('Moduladora')
-plt.xlim(0, 0.001)
+#plt.xlim(0, xlim)
 
 plt.subplot(3, 1, 3)
 plt.plot(sinal_am(t), 'b')
 plt.ylabel('Amplitude')
 plt.xlabel('Sinal AM')
-plt.xlim(0, 0.001)
+#plt.xlim(0, xlim)
 
 # plt.plot(t, portadora(t), color='red')
 plt.subplots_adjust(hspace=1)
